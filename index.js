ef2439125fa1629a78ccba9c33bdbcb2a8cf33e8
Yuvraj Maharshi <yogeshnda2018@gmail.com.com>
2023-04-23 20:16:00 +0530

    all set

--- index.js
+++ index.js
@@ -36,15 +36,7 @@ var el = document.getElementById("user-detail-name");
   var running = setTimeout(animate, 90);
 })();
 
-// // code sleep rep
-// // get the element
-//  const text1 = document.querySelector(".typing-text");
-
-// // make a words array
-//  const words1 = ["eat.", "sleep.", "code.", "repeat."];
-
-// // start typing effect
-// setTyper(text1, words1);
+ 
 
  const text = document.querySelector(".typing-dev");
 
@@ -112,76 +104,12 @@ function setTyper(element, words) {
 }
 
 
-// // main project bus 
-// document.getElementById("depbus").addEventListener("click", function () {
-//   window.open(
-//     "https://effortless-daffodil-5b9af6.netlify.app/",
-//     "_blank"
-//   );
-// });
-// document.getElementById("gitbus").addEventListener("click", function () {
-//   window.open(
-//     "https://github.com/krishna8464/-pricey-secretary-8466",
-//     "_blank"
-//   );
-// });
+ 
 
 
 
 
-
-// //1st prject github and deploy link
-// document.getElementById("gitindiamart").addEventListener("click", function () {
-//   window.open("https://github.com/krishna8464/-average-alarm-4307", "_blank");
-// });
-
-// document.getElementById("indmartdep").addEventListener("click", function () {
-//   window.open("https://mailtrap.netlify.app/", "_blank");
-// });
-
-//2nd prject github and deploy link
-// document.getElementById("depshopnow").addEventListener("click", function () {
-//   window.open(
-//     "https://get-a-hut.netlify.app/",
-//     "_blank"
-//   );
-// });
-// document.getElementById("gitshopnow").addEventListener("click", function () {
-//   window.open(
-//     "https://github.com/krishna8464/-dreary-hand-8237",
-//     "_blank"
-//   );
-// });
-
-//3rd prject github and deploy link
-
-// document.getElementById("gitweather").addEventListener("click", function () {
-//   window.open("https://github.com/krishna8464/Weatherapp", "_blank");
-// });
-// document.getElementById("depweather").addEventListener("click", function () {
-//   window.open("https://guileless-marshmallow-17f541.netlify.app", "_blank");
-// });
-
-
-
-// linkedin and github
-// document.getElementById("contact-linkedin").addEventListener("click", function () {
-//   window.open(
-//     "https://www.linkedin.com/in/yenda-vamshi-krishna-0826a2253/",
-//     "_blank"
-//   );
-// });
-// document.getElementById("contact-github").addEventListener("click", function () {
-//   window.open("https://github.com/krishna8464", "_blank");
-// });
-
-// document.getElementById("contact-emails").addEventListener("click", function () {
-//   window.open("mailto:yendavamshikrishna123@gmail.com", "_blank");
-// });
-
-// document.getElementById("contact-phones").addEventListener("click", function () {
-//   window.open("tel:8464963224", "_blank");
-// });
+ 
 
 
 //Resume button line
@@ -191,10 +119,10 @@ document.getElementById('resume-link-2').addEventListener("click",()=>{
 
  
 
+ 
+
+GitHubCalendar(".calendar", "Yuvraj1307");
+
 // or enable responsive functionality:
-GitHubCalendar(".calendar", "Yuvraj1307", { responsive: true , private:true});
-
-
-// function fun(){
-//   window.open("https://drive.google.com/file/d/14E1Rx27bgiBs6zp8RqJBsc3xOiKSRmHy/view?usp=sharing","_blank")
-// }
+GitHubCalendar(".calendar", "Yuvraj1307", { responsive: true });
+ 
